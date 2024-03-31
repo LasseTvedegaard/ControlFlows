bb15ae00a1a1bd57b30c2ee7f1bbade5dae20552
LasseTN <103989770+LasseTN@users.noreply.github.com>
2024-03-31 15:23:43 +0200

    Methods_to_Search_a_List_Contains__IndexOf__LastIndexOf_

---
+++ Search_List_Contains_IndexOf_LastIndexOf/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Methods_to_Search_a_List_Contains__IndexOf__LastIndexOf_ {
+    internal class Program {
+        static void Main(string[] args) {
+            List<int> list = new List<int>() { 5, 10, 26, 34, 2, 23, 200 };
+            if (list.Contains(5))   //Returns if the input number is in the list (yes/no)
+            {
+                Console.WriteLine("Found");
+
+            }
+
+            List<int> list1 = new List<int>() { 5, 10, 26, 34, 2, 23, 200 };
+            if (list1.IndexOf(23) == 5)   //Checks if the number 23 is on index 5
+            {
+                Console.WriteLine("true");
+            }
+
+            List<int> list2 = new List<int>() { 5, 10, 26, 34, 26, 26, 26, 2, 23, 26, 200 };
+            if (list2.LastIndexOf(26) == 9)   //Returns if the input number is in the list (yes/no)
+            {
+                Console.WriteLine("That´s right!");
+                Console.WriteLine(list2.LastIndexOf(26));   //Just a an output
+            }
+        }
+    }
+}
