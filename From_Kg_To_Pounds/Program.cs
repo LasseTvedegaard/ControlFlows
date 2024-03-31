18206534f1a2673e827386c55bbe153a39afc1d0
LasseTN <103989770+LasseTN@users.noreply.github.com>
2024-03-31 15:37:03 +0200

    From_Kg_to_Pounds

---
+++ From_Kg_To_Pounds/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace From_Kg_to_Pounds {
+    internal class FromKgToPoundsProgram {
+        public static string FromKgToPounds(double kg) {
+            double pounds;
+
+            pounds = kg * 2 + 0.20;
+
+            return $"{kg} kg is: ({pounds} pounds)";
+        }
+        static void Main(string[] args) {
+            Console.WriteLine(FromKgToPounds(1));
+            Console.WriteLine(FromKgToPounds(5));
+            Console.WriteLine(FromKgToPounds(83));
+            Console.WriteLine(FromKgToPounds(100));
+
+        }
+    }
+}
