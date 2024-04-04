96e7a5e4ce04f1e3d7725dcdc94f20de20f2d5a5
LasseTN <103989770+LasseTN@users.noreply.github.com>
2024-04-04 21:08:22 +0200

    firstAndLast

--- TestFlow/Program.cs
+++ TestFlow/Program.cs
@@ -7,14 +7,14 @@ using System.Text;
 namespace TestFlow {
     public class Program {
 
+
         static void Main(string[] args) {
-            int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14 };
-            for (int i = numbers.Length -1; i >= 0; i--) {
-                Console.WriteLine(numbers[i]);
-            }
+
         }
     }
+
 }
+
 
 
 
---
+++ firstAndLastNumberArray/Program.cs
@@ -0,0 +1,11 @@
+﻿static void Main(string[] args) {
+    int[] sortedArray = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+    int firstInteger = sortedArray[0];
+    int lastInteger = sortedArray[sortedArray.Length - 1];
+
+    Console.WriteLine("First Integer: " + firstInteger);
+    Console.WriteLine("Last Integer: " + lastInteger);
+}
+    
+        
